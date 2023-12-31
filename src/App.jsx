9c913fd799e91c01e8d2cae4b31c1f0b1de9f31b
Sahoo <artbangla21@gmail.com>
2024-01-01 00:53:30 +0530

    still working on the nav section and dark mode section is not included in this data 01/01/2024 time:00.53

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/navbar/Navbar";
 
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <>
-   <h2 className=" bg-orange-400 text-green-700">hello</h2>
+   <Navbar/>
      </>
   )
 }
